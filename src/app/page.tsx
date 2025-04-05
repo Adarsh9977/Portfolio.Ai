bd9ec2b710dc0042627c45ca64abdb160b4143d5
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2025-04-06 02:35:14 +0530

    title changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,6 @@ import { authOptions } from "@/lib/auth";
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
-  // if(!session){
-  //   redirect('/signin')
-  // }
-
   return (
     <div className="relative min-h-screen">
       <Navbar session={session}/>
